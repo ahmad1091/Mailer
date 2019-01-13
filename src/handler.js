a1a8c9f69b313f964e1848115c2ab44b0baf24b8
ahmad <ahmadtayeb91@gmail>
2019-01-13 12:46:01 +0200

    adding pages files to the structure related to #5

--- src/handler.js
+++ src/handler.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-  const handlePages=(target,request,response)=>{
-    const reqPage = {
-    signup: "public/signup/signup.html",
-    login: "public/login/login.html",
-    static: req.url
-    };
-    const filePath = path.join(__dirname, "..", reqPage[target]);
-    fs.readFile(filePath, (err, file) => {
-      res.writeHead(200);
-      if (err) throw err;
-      else {
-        res.end(file);
-      }
-    });
-  }
-
-  const handleHome = (target, req, res) => {
+const handlePages = (target, request, response) => {
   const reqPage = {
-  home: "public/index.html",
-  static: req.url
+    signup: 'public/signup/signup.html',
+    login: 'public/login/login.html',
+    static: req.url,
   };
-  fs.readFile(path.join(__dirname, "..", reqPage[target]), (err, file) => {
-  res.writeHead(200);
-  if (err) throw err;
-  else {
-  res.end(file);
-  }
+  const filePath = path.join(__dirname, '..', reqPage[target]);
+  fs.readFile(filePath, (err, file) => {
+    res.writeHead(200);
+    if (err) throw err;
+    else {
+      res.end(file);
+    }
   });
-  };
+};
 
-module.exports={handleHome,handlePages};
+const handleHome = (target, req, res) => {
+  const reqPage = {
+    home: 'public/home/index.html',
+    static: req.url,
+  };
+  fs.readFile(path.join(__dirname, '..', reqPage[target]), (err, file) => {
+    res.writeHead(200);
+    if (err) throw err;
+    else {
+      res.end(file);
+    }
+  });
+};
+
+module.exports = { handleHome, handlePages };
--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
-const {handleHome} = require('./handler');
+const { handleHome } = require('./handler');
 
 const router = (request, response) => {
-    const endPoint = request.url;
-    const method = request.method;
-    if (endPoint == "/" && method == "GET") {
-      handleHome("home", request, response);
-  } else if (endPoint.includes("home") && method == "GET") {
-      handleHome("static", request, response);
+  const endPoint = request.url;
+  const method = request.method;
+  if (endPoint == '/' && method == 'GET') {
+    handleHome('home', request, response);
+  } else if (endPoint.includes('home') && method == 'GET') {
+    handleHome('static', request, response);
   }
 };
 
-module.exports=router
+module.exports = router;
