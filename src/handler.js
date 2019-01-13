ab4aaa4fbda3cb89d77bedb41cf0668913ca9ae9
ahmad <ahmadtayeb91@gmail>
2019-01-13 10:31:08 +0200

    modify handler and router and install ESlint related to #12 #5

--- src/handler.js
+++ src/handler.js
@@ -1,39 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-  const handleHome=(request,response)=>{
-    response.writeHead(200,{"Content-Type":"text/html"});
-    fs.readFile(path.join(__dirname,'..','public','index.html'),(err,file)=>{
-      if (err) {
-        console.log(err);
-        return;
+  const handlePages=(target,request,response)=>{
+    const reqPage = {
+    signup: "public/signup/signup.html",
+    login: "public/login/login.html",
+    static: req.url
+    };
+    const filePath = path.join(__dirname, "..", reqPage[target]);
+    fs.readFile(filePath, (err, file) => {
+      res.writeHead(200);
+      if (err) throw err;
+      else {
+        res.end(file);
       }
-      response.end(file);
-    })
+    });
   }
 
-  const handleStatic=(request,response)=>{
-    const endPoint=request.url;
-    const extention=endPoint.split('.')[1];
-    console.log('extention and endPoint',endPoint,extention);
-    const contentType = {
-       html: 'text/html',
-       css: 'text/css',
-       js: 'application/javascript',
-       ico: 'image/x-icon',
-       jpg: 'image/jpg',
-       png: 'image/png',
-       jpeg: 'image/jpeg',
-     };
-     response.writeHead(200,{"contentType":contentType[extention]})
-fs.readFile(path.join(__dirname,'..',endPoint),(err,file)=>{
-  if (err) {
-    response.end(err.message)
+  const handleHome = (target, req, res) => {
+  const reqPage = {
+  home: "public/index.html",
+  static: req.url
+  };
+  fs.readFile(path.join(__dirname, "..", reqPage[target]), (err, file) => {
+  res.writeHead(200);
+  if (err) throw err;
+  else {
+  res.end(file);
   }
-  response.end(file);
-})
-     }
+  });
+  };
 
-
-
-module.exports={handleHome,handleStatic};
+module.exports={handleHome,handlePages};
